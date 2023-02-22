ff726125ec1b29a73faefdf300591ad84befa8d8
Kohei Hashmi <khashmi@student.unimelb.edu.au>
2023-02-22 15:45:22 +1100

    Init

---
+++ src/main/java/com/ko/showassist/ShowAssistApplication.java
@@ -0,0 +1,13 @@
+package com.ko.showassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ko/showassist/ShowAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ko.showassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
